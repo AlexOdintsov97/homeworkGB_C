36303af2e383dab2577bf103f1ae7cac0e781e66
AlexandrOdintsov <Odintsov.alexandr97@gmail.com>
2022-08-27 09:26:20 +0300

    Решил домашнее задание

---
+++ task19/task19.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+string? num = Console.ReadLine();
+
+void CheckingNum(string num)
+{
+  if (num[0]==num[4] || num[1]==num[3]){
+    Console.WriteLine($"Ваше число: {num} - палиндром.");
+  }
+  else Console.WriteLine($"Ваше число: {num} - не палиндром.");
+}
+
+if (num!.Length == 5){
+  CheckingNum(num);
+}
+else Console.WriteLine($"Введи правильное число");
---
+++ task23/task23.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите число: ");
+int cube = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int[] cube){
+  int i = 0;
+  int length = cube.Length;
+  while (i <  length){
+    cube[i] = Convert.ToInt32(Math.Pow(i, 3));
+    i++;
+  }
+}
+
+void PrintArry(int[] coll){
+  int count = coll.Length;
+  int i = 1;
+  while(i < count){
+    Console.Write(coll[i]+ " ");
+    i++;
+  }
+} 
+
+int[] arry = new int[cube+1];
+Cube(arry);
+PrintArry(arry);
